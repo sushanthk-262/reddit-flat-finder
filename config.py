f5a68afdfbc2a192faa43a4e377abeda45f84345
sushanthk-262 <92505351+sushanthk-262@users.noreply.github.com>
2026-02-09 10:16:44 +0530

    update config.py, add subreddits, filters to search

---
+++ config.py
@@ -0,0 +1,32 @@
+SUBREDDITS = [
+    "banglorerentals",
+    "flatandflatmatesblr",
+    "bangaloreflatsrental",
+    "flatmatesinbangalore",
+    "bangalorehousingmarket",
+    "bangaloreapartmentsforrent"
+]
+
+POST_LIMIT = 100
+
+KEYWORDS_REQUIRED = [
+    "2bhk",
+    "flatmate",
+    "available",
+    "rent",
+    "independent"
+]
+
+LOCATIONS = [
+    "bellandur",
+    "sarjapur",
+    "kasavanhalli",
+    "harlur",
+    "kadubesanahalli",
+    "devarabenahalli"
+]
+
+MIN_RENT = 10000
+MAX_RENT = 35000
+
+OUTPUT_FILE = "matches.csv"
